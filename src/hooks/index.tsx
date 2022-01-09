4ea573ec93f830ea32f5655eccbb2dad23e09e83
Augusto Iryoda <iryodap@gmail.com>
2022-01-09 16:45:55 -0300

    :sparkles: feat: react query provider

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,8 +1,13 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'styles/default.theme';
 
+const queryClient = new QueryClient();
+
 const AppProvider: React.FC = ({ children }) => (
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </QueryClientProvider>
 );
 
 export default AppProvider;
